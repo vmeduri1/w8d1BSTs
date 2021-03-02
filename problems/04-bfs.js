5b44de836bfef588eec79c5643ccef18bdb53fae
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-03-01 18:30:29 -0600

    Finished project

--- problems/04-bfs.js
+++ problems/04-bfs.js
@@ -4,6 +4,17 @@
 // in the order you visited them.
 function bfs(root){
     // Your code here
+    if (!root) return [];
+    let res = [];
+    let queue = [root];
+
+    while (queue.length) {
+        let current = queue.shift();
+        res.push(current.val);
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right);
+    }
+    return res;
 }
 
 module.exports = { bfs };
